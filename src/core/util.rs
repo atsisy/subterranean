fa8d229894d5f1cc79cb1ec36139b40a22285f0f
Akihiro Takai <at.sisy@gmail.com>
2020-01-30 10:51:48 +0900

    feat: 曜日に関する列挙体を定義

--- src/core/util.rs
+++ src/core/util.rs
@@ -5,3 +5,56 @@ pub fn year_to_season(year: i64) -> i64 {
 pub fn random_select<T>(mut i: std::slice::Iter<T>) -> Option<&T> {
     i.nth(rand::random::<usize>() % i.len())
 }
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum DayOfWeek {
+    Sunday,
+    Monday,
+    TuesDay,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+}
+
+impl DayOfWeek {
+    pub fn from_char(day_str: &str) -> Self {
+	match day_str {
+	    "Sun" => Self::Sunday,
+	    "Mon" => Self::Monday,
+	    "Tue" => Self::TuesDay,
+	    "Wed" => Self::Wednesday,
+	    "Thu" => Self::Thursday,
+	    "Fri" => Self::Friday,
+	    "Sat" => Self::Saturday,
+	    _ => Self::Sunday,
+	}
+    }
+
+    pub fn from_char_jp(day_str: &str) -> Self {
+	match day_str {
+	    "日曜日" => Self::Sunday,
+	    "月曜日" => Self::Monday,
+	    "火曜日" => Self::TuesDay,
+	    "水曜日" => Self::Wednesday,
+	    "木曜日" => Self::Thursday,
+	    "金曜日" => Self::Friday,
+	    "土曜日" => Self::Saturday,
+	    _ => Self::Sunday,
+	}
+    }
+}
+
+impl ToString for DayOfWeek {
+    fn to_string(&self) -> String {
+	match self {
+	    Self::Sunday => "日曜日",
+	    Self::Monday => "月曜日",
+	    Self::TuesDay => "火曜日",
+	    Self::Wednesday => "水曜日",
+	    Self::Thursday => "木曜日",
+	    Self::Friday => "金曜日",
+	    Self::Saturday => "土曜日",
+	}.to_string()
+    }
+}
