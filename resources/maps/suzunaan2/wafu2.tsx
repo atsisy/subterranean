3442b6e3580de566914c3cbc9b029fc5ebd1e86b
Akihiro Takai <at.sisy@gmail.com>
2020-06-13 15:28:19 +0900

    fix: マップ情報が読み取れないバグを修正

---
+++ resources/maps/suzunaan2/wafu2.tsx
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="wafu2" tilewidth="32" tileheight="32" tilecount="400" columns="8">
+ <image source="ST-Libry-I01.png" width="256" height="1600"/>
+ <tile id="163">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="0" width="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="165">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18" width="32" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18" width="32" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="167">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="18" width="32" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="173">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="3" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="174">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="175">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="181">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="21"/>
+  </objectgroup>
+ </tile>
+ <tile id="182">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="21"/>
+  </objectgroup>
+ </tile>
+ <tile id="183">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="21"/>
+  </objectgroup>
+ </tile>
+ <tile id="304">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="312">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="320">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="328">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="332">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="333">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="334">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="336">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="337">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="338">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="339">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="17"/>
+  </objectgroup>
+ </tile>
+</tileset>
